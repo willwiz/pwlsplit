244f021221e27083a4206881bdd0dad0d2ddf828
Will Zhang <willwz@gmail.com>
2025-11-30 17:20:16 -0500

    ENH: add validation for segmentdict

---
+++ src/pwlsplit/_validation.py
@@ -0,0 +1,25 @@
+# pyright: reportUnknownMemberType=false, reportUnknownVariableType=false
+from collections.abc import Mapping
+from typing import TYPE_CHECKING, TypeIs
+
+if TYPE_CHECKING:
+    from .trait import SegmentDict
+
+
+def is_segment_dict(obj: object) -> TypeIs[SegmentDict]:
+    if not isinstance(obj, Mapping):
+        return False
+    match obj.get("curve", None):
+        case "HOLD":
+            return True
+        case "STRETCH" | "RECOVER":
+            required_vars = ["delta", "time"]
+        case _:
+            return False
+    for var in required_vars:
+        match obj.get(var):
+            case int() | float():
+                continue
+            case _:
+                return False
+    return True
---
+++ src/pwlsplit/api.py
@@ -0,0 +1,3 @@
+from ._validation import is_segment_dict
+
+__all__ = ["is_segment_dict"]
